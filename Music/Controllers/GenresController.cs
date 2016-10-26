36a75b6ea86a46d6e3b5782d70bc54cbd3c09f7e
Brian's <Brian's@BRIANS-PC>
2016-10-25 22:18:13 -0400

    Add project files.

---
+++ Music/Controllers/GenresController.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Music.Models;
+
+namespace Music.Controllers
+{
+    public class GenresController : Controller
+    {
+        private MusicContext db = new MusicContext();
+        // GET: Genre
+        public ActionResult Index()
+        {
+            var something = db.Genres.ToList();
+            return View(something);
+        }
+        // GET: Genre/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genres/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GenreID, Name")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(genre);
+        }
+
+
+
+
+
+
+
+
+
+
+
+        [HttpGet]
+        public ActionResult SortByGenre(int? id)
+        {
+
+
+           
+
+            if (id != null)
+            {
+
+                var genre = db.Genres.Where(a => a.GenreID == id);
+                ViewBag.header = genre.First().Name;
+                var albums = db.Albums
+                    .Include(a => a.Artist)
+                    .Include(a => a.Genre)
+                    .Where(a => a.GenreID == id);
+                return View("GenreSort", albums.ToList());
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        
+        
+
+
+
+
+
+
+
+    }
+    }
+
---
+++ Music/Models/Album.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Music.Models
+{
+    public class Album
+    {
+
+        public int AlbumID { get; set; }
+        [Required(ErrorMessage ="Album title is required")]
+        public string Title { get; set; }
+        
+        public int GenreID { get; set; }
+        [Display(Name = "Genre")]
+        public Genre Genre { get; set; }
+        public decimal Price { get; set; }
+
+        public int ArtistID { get; set; }
+        [Display(Name = "Artist")]
+        public Artist Artist { get; set; }
+
+    }
+}
